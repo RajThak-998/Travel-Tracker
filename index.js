761cf41750fc76fb86a07a77b1747aaf97dda280
Rajdev Singh <rajdsingh998@gmail.com>
2025-06-28 16:17:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const db = new pg.Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-// Connect to database
-db.connect();
+db.connect();  
+
+export default db;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
